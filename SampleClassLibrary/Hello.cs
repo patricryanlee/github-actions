eb41a82abbd865a8af3e6d0e579e9d92e28e2a9d
Patric Lee <patricryanlee@gmail.com>
2024-01-29 13:45:05 +1300

    first commit

---
+++ SampleClassLibrary/Hello.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleClassLibrary;
+
+public class Hello
+{
+    private readonly string _word;
+    public Hello(string word)
+    {
+        _word = word;
+    }
+
+    public string Print()
+    {
+        return $"Hello {_word}";
+    }
+}
